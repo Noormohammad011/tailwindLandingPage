188736adcaf0ad666fd7e022b7a45e0b783b5c02
Noormohammad011 <noormohammad.net1503011@gmail.com>
2021-07-07 16:11:18 +0600

    redux setup is completed

--- tailwind-landing-page-template/src/index.js
+++ tailwind-landing-page-template/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
+import { Provider } from 'react-redux'
+import store from './store'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
